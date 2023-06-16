fab2cc4747b031e212e91ce7135ae3d3bcae0939
SentinelAssault <94572905+SentinelAssault@users.noreply.github.com>
2023-06-15 22:45:40 -0500

    Basic Subsystem Classes Added, empty

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,14 +4,15 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.SwerveDrive;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
-  public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  public static CommandBase exampleAuto(SwerveDrive subsystem) {
+    return Commands.sequence(subsystem.exampleMethodCommand(), new RobotCentricDrive(subsystem));
   }
 
   private Autos() {
